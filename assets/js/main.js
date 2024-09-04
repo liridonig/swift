1963df0e34d8d463893741f100995d2b46b7be19
Liridon Gashi <doo.nii@hotmail.com>
2024-09-04 22:14:54 +0200

    Testomianls Fixed Slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,9 @@
 // toggle navbar
 document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.querySelector(".hamburger");
-    const navbar = document.querySelector(".navbar");
-  
-    hamburger.addEventListener("click", function () {
-      navbar.classList.toggle("active-nav");
-    });
-  });
+  const hamburger = document.querySelector(".hamburger");
+  const navbar = document.querySelector(".navbar");
 
-  
+  hamburger.addEventListener("click", function () {
+    navbar.classList.toggle("active-nav");
+  });
+});
